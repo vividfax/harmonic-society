37b57f377d5eb575a2c9625416394fd4de5ae6d4
vividfax <vividfax@gmail.com>
2021-03-31 17:09:15 +0100

    Add tone.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let buttons = [];
 let qwerty = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+let players = [];
+let loops = [];
 
 function setup() {
 
@@ -7,9 +9,10 @@ function setup() {
 	background("#333");
 
 	createButtons();
-
 	buttons[4].status = "inactive";
 	buttons[25].status = "inactive";
+
+	createSoundPlayer();
 }
 
 function draw() {
@@ -28,8 +31,10 @@ function keyTyped() {
 
 			if (buttons[i].status == "inactive") {
 				buttons[i].status = "active";
+				playSound(i);
 			} else if (buttons[i].status == "active") {
 				buttons[i].status = "inactive";
+				stopSound(i);
 			}
 		}
 	}
@@ -51,4 +56,30 @@ buttons[i] = new Button(startPoint + (65+padding) * (i - 10), height/2 + padding
 	for (let i = 19; i < 26; i++) {
 buttons[i] = new Button(startPoint + (65+padding) * (i - 19), height/2 + padding * 2 + 65 * 2, qwerty[i]);
 	}
+}
+
+function createSoundPlayer() {
+
+	for (let i = 0; i < qwerty.length; i++) {
+
+		players[i] = new Tone.Player({
+			url: "samples/" + qwerty[i] + ".wav",
+		}).toMaster();
+	}
+}
+
+function playSound(i) {
+
+	loops[i] = new Tone.Loop((time) => {
+
+		players[i].start();
+		console.log(time);
+
+	}, "1n").start(i%4);
+	Tone.Transport.start();
+}
+
+function stopSound(i) {
+
+	loops[i].stop();
 }
